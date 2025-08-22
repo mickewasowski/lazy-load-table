038d5f1c252664c89fc480d8d10ab4d1bf3a867f
Rosen Yorgov <yorgov.h.r@gmail.com>
2025-08-22 21:27:04 +0300

    testing with dummy data

---
+++ src/dummy.ts
@@ -0,0 +1,402 @@
+export const data = [
+  {
+    position: 1,
+    text: "WBdz0wVl8S",
+  },
+  {
+    position: 2,
+    text: "Yq4PTDVpvc",
+  },
+  {
+    position: 3,
+    text: "fJVWtH9WSW",
+  },
+  {
+    position: 4,
+    text: "PtC2yDdkP7",
+  },
+  {
+    position: 5,
+    text: "onO2U4nXMa",
+  },
+  {
+    position: 6,
+    text: "lSqRVVwi5u",
+  },
+  {
+    position: 7,
+    text: "h9bBL8u3rA",
+  },
+  {
+    position: 8,
+    text: "cp6xzH4N2y",
+  },
+  {
+    position: 9,
+    text: "3TYflw2dOb",
+  },
+  {
+    position: 10,
+    text: "QdRA0O9B1H",
+  },
+  {
+    position: 11,
+    text: "yNZqL3GsiR",
+  },
+  {
+    position: 12,
+    text: "BaqnrjzDho",
+  },
+  {
+    position: 13,
+    text: "Z5gMq07hbi",
+  },
+  {
+    position: 14,
+    text: "Hs2MFEG0by",
+  },
+  {
+    position: 15,
+    text: "P1wY4mEhvS",
+  },
+  {
+    position: 16,
+    text: "LdZ57J6Ium",
+  },
+  {
+    position: 17,
+    text: "9RDL2E3CkW",
+  },
+  {
+    position: 18,
+    text: "u1nl08eWsz",
+  },
+  {
+    position: 19,
+    text: "K9dOjUFltA",
+  },
+  {
+    position: 20,
+    text: "Gc48MDPpSh",
+  },
+  {
+    position: 21,
+    text: "dFq0G7i8SW",
+  },
+  {
+    position: 22,
+    text: "lxg1wQHNjB",
+  },
+  {
+    position: 23,
+    text: "A3Dq5Wwn2U",
+  },
+  {
+    position: 24,
+    text: "C0X4N9ipzK",
+  },
+  {
+    position: 25,
+    text: "i7rQ8shFcA",
+  },
+  {
+    position: 26,
+    text: "mHh1ltD3fO",
+  },
+  {
+    position: 27,
+    text: "xVwa9rE1Sy",
+  },
+  {
+    position: 28,
+    text: "5oVrkIefpw",
+  },
+  {
+    position: 29,
+    text: "Pp0bAdMksj",
+  },
+  {
+    position: 30,
+    text: "TYqCe8sKn1",
+  },
+  {
+    position: 31,
+    text: "2sA6T7nKZC",
+  },
+  {
+    position: 32,
+    text: "gtZDpQn1UO",
+  },
+  {
+    position: 33,
+    text: "8qszlaTfOB",
+  },
+  {
+    position: 34,
+    text: "VxY3oO9iXc",
+  },
+  {
+    position: 35,
+    text: "FqL5PrdiV8",
+  },
+  {
+    position: 36,
+    text: "Ruhd1qC9Ke",
+  },
+  {
+    position: 37,
+    text: "lXkQ76NnsO",
+  },
+  {
+    position: 38,
+    text: "aCtVj2pM8F",
+  },
+  {
+    position: 39,
+    text: "Km20lxGTfa",
+  },
+  {
+    position: 40,
+    text: "NsD7MYbQ3z",
+  },
+  {
+    position: 41,
+    text: "cwQ7K4mUGY",
+  },
+  {
+    position: 42,
+    text: "ZnsP3oHVAm",
+  },
+  {
+    position: 43,
+    text: "V39kFR0lTm",
+  },
+  {
+    position: 44,
+    text: "C8gkZpSNqD",
+  },
+  {
+    position: 45,
+    text: "yznl3t9iDC",
+  },
+  {
+    position: 46,
+    text: "mD9wAqF2hP",
+  },
+  {
+    position: 47,
+    text: "ZXm7o2RG9t",
+  },
+  {
+    position: 48,
+    text: "Fnj8czYOTu",
+  },
+  {
+    position: 49,
+    text: "7iDkYwfjHC",
+  },
+  {
+    position: 50,
+    text: "QfSZ4KuBVt",
+  },
+  {
+    position: 51,
+    text: "X7GOhqVdF2",
+  },
+  {
+    position: 52,
+    text: "iFj4d9NVMt",
+  },
+  {
+    position: 53,
+    text: "h6RO9axzBf",
+  },
+  {
+    position: 54,
+    text: "t0B3jF4NvP",
+  },
+  {
+    position: 55,
+    text: "Kw1tZVx7FR",
+  },
+  {
+    position: 56,
+    text: "rAV0ojxT2S",
+  },
+  {
+    position: 57,
+    text: "n3udRl4v6K",
+  },
+  {
+    position: 58,
+    text: "z9cON3tbhG",
+  },
+  {
+    position: 59,
+    text: "Tk1vrfD23M",
+  },
+  {
+    position: 60,
+    text: "R9OTy3vXeB",
+  },
+  {
+    position: 61,
+    text: "xLM3CqUvYd",
+  },
+  {
+    position: 62,
+    text: "gWb7R0nZFc",
+  },
+  {
+    position: 63,
+    text: "U8nF1pB7vj",
+  },
+  {
+    position: 64,
+    text: "MLwcFz0uHJ",
+  },
+  {
+    position: 65,
+    text: "jWdp6EN1oC",
+  },
+  {
+    position: 66,
+    text: "B5ckF0Oxqw",
+  },
+  {
+    position: 67,
+    text: "t7s4bQ3iVY",
+  },
+  {
+    position: 68,
+    text: "xMC3e95Xfg",
+  },
+  {
+    position: 69,
+    text: "P79ZkX0vAc",
+  },
+  {
+    position: 70,
+    text: "ibn6tEKxJ7",
+  },
+  {
+    position: 71,
+    text: "VQZ7Toh3Gb",
+  },
+  {
+    position: 72,
+    text: "lmM0N7KhbP",
+  },
+  {
+    position: 73,
+    text: "eP0Luz8xBh",
+  },
+  {
+    position: 74,
+    text: "CcUB4SkKfO",
+  },
+  {
+    position: 75,
+    text: "kzV2cnUhym",
+  },
+  {
+    position: 76,
+    text: "5QyBzxF79S",
+  },
+  {
+    position: 77,
+    text: "H7Y2hmfs9k",
+  },
+  {
+    position: 78,
+    text: "mQ7klPwhNC",
+  },
+  {
+    position: 79,
+    text: "AYV5lZkGhm",
+  },
+  {
+    position: 80,
+    text: "3tRcU7LJ6h",
+  },
+  {
+    position: 81,
+    text: "ofM0zA7wGy",
+  },
+  {
+    position: 82,
+    text: "ZwJp3gX7fN",
+  },
+  {
+    position: 83,
+    text: "E4bgOpkn2u",
+  },
+  {
+    position: 84,
+    text: "jqvM7SH6tX",
+  },
+  {
+    position: 85,
+    text: "2CuhV1pRgo",
+  },
+  {
+    position: 86,
+    text: "mTY1Er0iXn",
+  },
+  {
+    position: 87,
+    text: "KJ0lq4WdPR",
+  },
+  {
+    position: 88,
+    text: "dB4UPkFaw3",
+  },
+  {
+    position: 89,
+    text: "ZP4ATiN7Xv",
+  },
+  {
+    position: 90,
+    text: "ryV8ShuZz6",
+  },
+  {
+    position: 91,
+    text: "FjvnTz8Ckp",
+  },
+  {
+    position: 92,
+    text: "2A0ldH1yoC",
+  },
+  {
+    position: 93,
+    text: "beK3sVf1xR",
+  },
+  {
+    position: 94,
+    text: "4uNfGc1BoK",
+  },
+  {
+    position: 95,
+    text: "Zr2Bpf0CVM",
+  },
+  {
+    position: 96,
+    text: "L8hfpKdQmT",
+  },
+  {
+    position: 97,
+    text: "oEavP6DYjR",
+  },
+  {
+    position: 98,
+    text: "Np1rEZ3dB8",
+  },
+  {
+    position: 99,
+    text: "S9vqR7nMpc",
+  },
+  {
+    position: 100,
+    text: "qLtjE2sPVa",
+  },
+];
